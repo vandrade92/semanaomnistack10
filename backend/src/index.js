1677f43c50e171a04d61cb06c1cc41b9127aa03f
Vagner Andrade <vagner.matias@gmail.com>
2020-01-21 22:24:02 -0300

    Aula InterfaceWeb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes.js');
 
 const app = express();
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-aau3x.mongodb.net/w
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
